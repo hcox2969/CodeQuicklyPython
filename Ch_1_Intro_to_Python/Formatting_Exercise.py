6bc86519793816ffa1f9b9ef0e9b8b34e1515254
helen2969 <>
2021-02-27 20:16:10 -0500

    formatting

---
+++ Ch_1_Intro_to_Python/Formatting_Exercise.py
@@ -0,0 +1,13 @@
+#FORMATTING EXERCISE
+#1. assign a numberical value to variable
+R = 9
+#2. change the value in some way
+R = 9/3
+#3. Assign a string value to some variable
+stringy = "There will be a nuimber following tis sentence: {}".format(R)
+#4. print the string and then the value by using string formatting:
+print(stringly)
+#5. Convert the numerical data into a different format:
+R = str(R)
+#6. pring the new data form:
+print(R)
