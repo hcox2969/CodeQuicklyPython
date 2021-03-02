0d3618c38f6197e41f52660e61297dfc078d3a67
helen2969 <>
2021-03-01 20:16:37 -0500

    inputs

---
+++ Ch_1_Intro_to_Python/Inputs_Formating_Outputs.py
@@ -0,0 +1,17 @@
+
+
+#favorite_food = input("What is your favorite food?: ")
+
+
+user_name = "Helen"
+favorite_food = input("What is ()'s favorite food?: ").format("user_name")
+
+#Write a prgoram that does the following:
+#1. Prompts the user for answers to several different questions
+favorite_food= input("What's your favorite food?: ")
+favorite_animal = input("What about yhour favorite animal?: ")
+favorite_movie = input("What's the best movie?: ")
+#2. Prints out the answers on different lines using a single print statement
+print("Favorite food is: " + favorite_food + "\n" +
+        "Favorite animal is: " +  favorite_animal + "\n" +
+        "Favorite movie is: " + favorite_movie)
